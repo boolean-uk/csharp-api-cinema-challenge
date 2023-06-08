441eadc1f1af0de37c77f6e8b9b3a87293c5fbdd
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-06-08 19:05:05 +0300

    update

--- api-cinema-challenge/api-cinema-challenge/Repositories/CinemaRepo.cs
+++ api-cinema-challenge/api-cinema-challenge/Repositories/CinemaRepo.cs
@@ -1,12 +1,14 @@
 ﻿using api_cinema_challenge.Data;
 using api_cinema_challenge.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace api_cinema_challenge.Repositories
 {
     public class CinemaRepo : ICinemaRepo
     {
+
+
+        // Customers
         public Customer CreateCustomer(Customer customer)
         {
             using (var db = new CinemaContext())
@@ -18,6 +20,64 @@ namespace api_cinema_challenge.Repositories
 
         }
 
+        public Customer DeleteCustomer(int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var deleteCusomer = db.Customers.FirstOrDefault(c => c.Id == id);
+                if (deleteCusomer != null)
+                {
+                    db.Customers.Remove(deleteCusomer);
+                    db.SaveChanges();
+                    return deleteCusomer;
+
+                }
+                return null;
+            }
+        }
+        public Customer UpdateCustomer(Customer customer, int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var updatedCustomer = db.Customers.Include(x => x.Ticket).ThenInclude(x => x.screening).ThenInclude(x => x.movie).FirstOrDefault(c => c.Id == id);
+                if (updatedCustomer != null)
+                {
+                    updatedCustomer = customer;
+                    // updatedCustomer.Id = id;
+                    // updatedCustomer.Name = customer.Name;
+                    // updatedCustomer.Email = customer.Email;
+                    // updatedCustomer.Phone = customer.Phone;
+                    // updatedCustomer.UpdatedDate = DateTime.UtcNow;
+                    db.SaveChanges();
+                    return updatedCustomer;
+
+                }
+                return null;
+            }
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            using (var db = new CinemaContext())
+            {
+                return db.Customers.Include(x => x.Ticket).ThenInclude(x => x.screening).ThenInclude(x => x.movie).ToList();
+            }
+        }
+        public Customer GetCustomerById(int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var customer = db.Customers.Include(x => x.Ticket).ThenInclude(x => x.screening).ThenInclude(x => x.movie).FirstOrDefault(c => c.Id == id);
+                if (customer != null)
+                {
+
+                    return customer;
+
+                }
+                return null;
+            }
+        }
+        //Movies
         public Movie CreateMovie(Movie movie)
         {
             using (var db = new CinemaContext())
@@ -28,41 +88,7 @@ namespace api_cinema_challenge.Repositories
             }
         }
 
-        public Screening CreateScreening(Screening screen)
-        {
-            using (var db = new CinemaContext())
-            {
-                db.Screenings.Add(screen);
-                db.SaveChanges();
-                return screen;
-            }
-        }
 
-        public Ticket CreateTicket(Ticket ticket)
-        {
-            using (var db = new CinemaContext())
-            {
-                db.Tickets.Add(ticket);
-                db.SaveChanges();
-                return ticket;
-            }
-        }
-
-        public Customer DeleteCustomer(int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var deleteCusomer = db.Customers.FirstOrDefault(c => c.Id == id);
-                if (deleteCusomer != null)
-                {
-                    db.Customers.Remove(deleteCusomer);
-                    db.SaveChanges() ;
-                    return deleteCusomer;
-
-                }
-                return null;
-            }
-        }
 
         public Movie DeleteMovie(int id)
         {
@@ -79,7 +105,121 @@ namespace api_cinema_challenge.Repositories
                 return null;
             }
         }
+        public Movie GetMovieById(int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var movie = db.Movies.FirstOrDefault(c => c.Id == id);
+                if (movie != null)
+                {
 
+                    return movie;
+
+                }
+                return null;
+            }
+        }
+        public IEnumerable<Movie> GetallMovies()
+        {
+            using (var db = new CinemaContext())
+            {
+                return db.Movies.ToList();
+            }
+        }
+        public Movie UpdateMovie(Movie movie, int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var updatedMovie = db.Movies.FirstOrDefault(c => c.Id == id);
+                if (updatedMovie != null)
+                {
+                    updatedMovie = movie;
+                    //updatedMovie.Id = id;
+                    //updatedMovie.runtime = movie.runtime;
+                    //updatedMovie.description = movie.description;
+                    //updatedMovie.tittle = movie.tittle;
+                    //updatedMovie.UpdatedAt = DateTime.UtcNow;
+                    // updatedMovie.rating = movie.rating;
+                    db.SaveChanges();
+                    return updatedMovie;
+
+                }
+                return null;
+            }
+
+        }
+
+
+        // Tickets
+        public IEnumerable<Ticket> GetallTicket()
+        {
+            using (var db = new CinemaContext())
+            {
+                return db.Tickets.Include(t => t.screening).ThenInclude(e => e.movie).ToList();
+            }
+        }
+        public Ticket CreateTicket(Ticket ticket)
+        {
+            using (var db = new CinemaContext())
+            {
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+                return ticket;
+            }
+        }
+
+        // Screenings
+        public Screening UpdateScreening(Screening screen, int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var updatedScreen = db.Screenings.Include(x => x.movie).FirstOrDefault(c => c.Id == id);
+                if (updatedScreen != null)
+                {
+                    updatedScreen = screen;
+                    // updatedScreen.Id = id;
+                    // updatedScreen.capacity = screen.capacity;
+
+                    // updatedScreen.screenNumber = screen.screenNumber;
+                    //updatedScreen.startsAt = screen.startsAt;
+                    // updatedScreen.UpdatedAt = DateTime.UtcNow;
+
+                    db.SaveChanges();
+                    return updatedScreen;
+
+                }
+                return null;
+            }
+        }
+        public Screening GetScreeningById(int id)
+        {
+            using (var db = new CinemaContext())
+            {
+                var screen = db.Screenings.Include(x => x.movie).FirstOrDefault(c => c.Id == id);
+                if (screen != null)
+                {
+
+                    return screen;
+
+                }
+                return null;
+            }
+        }
+
+        public IEnumerable<Screening> GetallScreenings()
+        {
+            using (var db = new CinemaContext())
+            {
+                foreach (var c in db.Screenings)
+                {
+                    string date = c.startsAt.ToString("yyyy-MM-dd HH:mm:ss");
+                    c.startsAt = DateTime.Parse(date);
+
+                }
+                return db.Screenings.Include(e => e.movie).ToList();
+            }
+
+        }
         public Screening DeleteScreening(int id)
         {
             using (var db = new CinemaContext())
@@ -95,155 +235,13 @@ namespace api_cinema_challenge.Repositories
                 return null;
             }
         }
-
-        public IEnumerable<Customer> GetAllCustomers()
-        {
-            using(var db = new CinemaContext())
-            {
-                return db.Customers.Include(x=> x.Ticket).ThenInclude(x=>x.screening).ThenInclude(x => x.movie).ToList();
-            }
-        }
-
-        public IEnumerable<Movie> GetallMovies()
+        public Screening CreateScreening(Screening screen)
         {
             using (var db = new CinemaContext())
             {
-                return db.Movies.ToList();
-            }
-        }
-
-        public IEnumerable<Screening> GetallScreenings()
-        {
-            using (var db = new CinemaContext())
-            {
-                foreach (var c in db.Screenings)
-                {
-                    string date = c.startsAt.ToString("yyyy-MM-dd HH:mm:ss");
-                    c.startsAt = DateTime.Parse(date);
-
-                }
-                return db.Screenings.Include(e=> e.movie).ToList();
-            }
-
-        }
-
-        public IEnumerable<Ticket> GetallTicket()
-        {
-            using (var db = new CinemaContext())
-            {
-                return db.Tickets.Include(t => t.screening).ThenInclude(e => e.movie).ToList();
-            }
-        }
-
-        public Customer GetCustomerById(int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var customer = db.Customers.Include(x => x.Ticket).ThenInclude(x => x.screening).ThenInclude(x => x.movie).FirstOrDefault(c => c.Id == id);
-                if (customer != null)
-                {
-                    
-                    return customer;
-
-                }
-                return null;
-            }
-        }
-
-        public Movie GetMovieById(int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var movie = db.Movies.FirstOrDefault(c => c.Id == id);
-                if (movie != null)
-                {
-
-                    return movie;
-
-                }
-                return null;
-            }
-        }
-
-        public Screening GetScreeningById(int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var screen = db.Screenings.Include(x=>x.movie).FirstOrDefault(c => c.Id == id);
-                if (screen != null)
-                {
-
-                    return screen;
-
-                }
-                return null;
-            }
-        }
-
-        public Customer UpdateCustomer(Customer customer, int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var updatedCustomer = db.Customers.Include(x => x.Ticket).ThenInclude(x => x.screening).ThenInclude(x => x.movie).FirstOrDefault(c => c.Id == id);
-                if (updatedCustomer != null)
-                {
-                    updatedCustomer = customer;
-                   // updatedCustomer.Id = id;
-                   // updatedCustomer.Name = customer.Name;
-                   // updatedCustomer.Email = customer.Email;
-                   // updatedCustomer.Phone = customer.Phone;
-                   // updatedCustomer.UpdatedDate = DateTime.UtcNow;
-                    db.SaveChanges();
-                    return updatedCustomer;
-
-                }
-                return null;
-            }
-        }
-
-        public Movie UpdateMovie(Movie movie, int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var updatedMovie = db.Movies.FirstOrDefault(c => c.Id == id);
-                if (updatedMovie != null)
-                {
-                    updatedMovie = movie;
-                    //updatedMovie.Id = id;
-                    //updatedMovie.runtime = movie.runtime;
-                    //updatedMovie.description = movie.description;
-                    //updatedMovie.tittle = movie.tittle;
-                    //updatedMovie.UpdatedAt = DateTime.UtcNow;
-                   // updatedMovie.rating = movie.rating;
-                    db.SaveChanges();
-                    return updatedMovie;
-
-                }
-                return null;
-            }
-
-        }
-
-        public Screening UpdateScreening(Screening screen, int id)
-        {
-            using (var db = new CinemaContext())
-            {
-                var updatedScreen = db.Screenings.Include(x=> x.movie).FirstOrDefault(c => c.Id == id);
-                if (updatedScreen != null)
-                {
-                    updatedScreen = screen;
-                   // updatedScreen.Id = id;
-                   // updatedScreen.capacity = screen.capacity;
-
-                   // updatedScreen.screenNumber = screen.screenNumber;
-                   //updatedScreen.startsAt = screen.startsAt;
-                   // updatedScreen.UpdatedAt = DateTime.UtcNow;
-                    
-                    db.SaveChanges();
-                    return updatedScreen;
-
-                }
-                return null;
+                db.Screenings.Add(screen);
+                db.SaveChanges();
+                return screen;
             }
         }
     }
