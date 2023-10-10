6c21a5dc22c7d6cd30f70f3671940379a8d84bd2
konas <konas@LAPTOP-1GHMR8NV>
2023-10-10 12:27:42 +0300

    added Models

---
+++ api-cinema-challenge/api-cinema-challenge/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace api_cinema_challenge.Models.Customer
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+    }
+}
---
+++ api-cinema-challenge/api-cinema-challenge/Models/Screening.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_cinema_challenge.Models.Screening
+{
+    public class Screening
+    {
+        public int id { get; set; }
+        public int screenNumber { get; set; }
+        public int capacity { get; set; }
+        public DateTime startsAt { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+
+        [ForeignKey("Movie")]
+        public int movieId { get; set; }
+    }
+}
