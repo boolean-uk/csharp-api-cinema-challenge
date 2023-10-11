bc472a49a99188dd9dfa00f8dd14ea98a20f11da
konas <konas@LAPTOP-1GHMR8NV>
2023-10-11 13:21:54 +0300

    created Ticket model

---
+++ api-cinema-challenge/api-cinema-challenge/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_cinema_challenge.Models
+{
+    public class Ticket
+    {
+        public int id { get; set; }
+        public int numSeats { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+
+        [ForeignKey("Customer")]
+        public int customerId { get; set; }
+
+        [ForeignKey("Screening")]
+        public int screeningId { get; set; }
+    }
+}
---
+++ api-cinema-challenge/api-cinema-challenge/Models/TicketPost.cs
@@ -0,0 +1,7 @@
+﻿namespace api_cinema_challenge.Models
+{
+    public class TicketPost
+    {
+        public required int numSeats { get; set; }
+    }
+}
