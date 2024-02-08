b0ad192f990ee784b94ac29eac06063f8cf8de58
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-08 16:28:13 +0100

    Needed to purge migrations

--- api-cinema-challenge/api-cinema-challenge/Migrations/20240208151939_updatedDatabaseSeeder.Designer.cs
+++ api-cinema-challenge/api-cinema-challenge/Migrations/20240208151939_updatedDatabaseSeeder.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using api_cinema_challenge.Data;
@@ -11,9 +12,11 @@ using api_cinema_challenge.Data;
 namespace api_cinema_challenge.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240208151939_updatedDatabaseSeeder")]
+    partial class updatedDatabaseSeeder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
