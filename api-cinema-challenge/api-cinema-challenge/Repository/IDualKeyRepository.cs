a350f65ff9295a05eda453c91cb1cdc12a2d963c
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-09 15:15:31 +0100

    Added Generic Repository

---
+++ api-cinema-challenge/api-cinema-challenge/Repository/IDualKeyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace api_cinema_challenge.Repository
+{
+    public interface IDualKeyRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T?> GetByID(object id1, object id2);
+        Task<T> Insert(T entity);
+        Task<T> Update(T entity);
+        Task<T?> DeleteByID(object id1, object id2);
+    }
+}
