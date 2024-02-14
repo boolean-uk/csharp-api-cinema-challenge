589d148a94f079b6c360e31d659aab9dc703d637
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-14 14:10:53 +0100

    Minor docker config change

--- api-cinema-challenge/api-cinema-challenge/Data/DataContext.cs
+++ api-cinema-challenge/api-cinema-challenge/Data/DataContext.cs
@@ -10,9 +10,7 @@ namespace api_cinema_challenge.Data
         private string _connectionString;
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
-            this.Database.EnsureCreated();
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
