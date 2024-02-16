c273a2d3c6dc48ba4bfd2e78a2e8b83983f3257a
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-02-16 11:53:39 +0100

    add the lost diagram

--- sql-schema.sql
+++ sql-schema.sql
@@ -1,49 +1,63 @@
-CREATE TABLE Customers (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(50) NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE Customers
+(
+  id         SERIAL       NULL    ,
+  name       VARCHAR(255) NOT NULL,
+  email      VARCHAR(255) NOT NULL,
+  phone      VARCHAR(50)  NOT NULL,
+  created_at TIME         NULL     DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIME         NULL     DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
-CREATE TABLE Movies (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    rating VARCHAR(50),
-    description TEXT,
-    runtimeMins INTEGER NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
+ALTER TABLE Customers
+  ADD CONSTRAINT UQ_email UNIQUE (email);
+
+CREATE TABLE Movies
+(
+  id           SERIAL       NULL    ,
+  title        VARCHAR(255) NOT NULL,
+  rating       VARCHAR(50)  NULL    ,
+  description  TEXT         NULL    ,
+  runtime_mins INTEGER      NOT NULL,
+  created_at   TIME         NULL     DEFAULT CURRENT_TIMESTAMP,
+  updated_at   TIME         NULL     DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
-CREATE TABLE Screenings (
-    id SERIAL PRIMARY KEY,
-    movieId INTEGER NOT NULL REFERENCES Movies(id) ON DELETE CASCADE,
-    screenNumber INTEGER NOT NULL,
-    capacity INTEGER NOT NULL,
-    startsAt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE Screenings
+(
+  id            SERIAL  NULL    ,
+  movie_id      INTEGER NOT NULL,
+  screen_number INTEGER NOT NULL,
+  capacity      INTEGER NOT NULL,
+  starts_at     TIME    NOT NULL,
+  created_at    TIME    NULL     DEFAULT CURRENT_TIMESTAMP,
+  updated_at    TIME    NULL     DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
-CREATE TABLE Tickets (
-    id SERIAL PRIMARY KEY,
-    customerId INTEGER NOT NULL REFERENCES Customers(id) ON DELETE CASCADE,
-    screeningId INTEGER NOT NULL REFERENCES Screenings(id) ON DELETE CASCADE,
-    numSeats INTEGER NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE Tickets
+(
+  fk_customer_id  INTEGER NOT NULL,
+  fk_screening_id INTEGER NOT NULL,
+  num_seats       INTEGER NOT NULL,
+  created_at      TIME    NULL     DEFAULT CURRENT_TIMESTAMP,
+  updated_at      TIME    NULL     DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (fk_customer_id, fk_screening_id)
 );
+
 ALTER TABLE Screenings
-ADD CONSTRAINT fk_movie
-FOREIGN KEY (movieId) REFERENCES Movies(id)
-ON DELETE CASCADE;
+  ADD CONSTRAINT FK_Movies_TO_Screenings
+    FOREIGN KEY (movie_id)
+    REFERENCES Movies (id);
 
 ALTER TABLE Tickets
-ADD CONSTRAINT fk_customer
-FOREIGN KEY (customerId) REFERENCES Customers(id)
-ON DELETE CASCADE;
+  ADD CONSTRAINT FK_Customers_TO_Tickets
+    FOREIGN KEY (fk_customer_id)
+    REFERENCES Customers (id);
 
 ALTER TABLE Tickets
-ADD CONSTRAINT fk_screening
-FOREIGN KEY (screeningId) REFERENCES Screenings(id)
-ON DELETE CASCADE;
-
+  ADD CONSTRAINT FK_Screenings_TO_Tickets
+    FOREIGN KEY (fk_screening_id)
+    REFERENCES Screenings (id);
