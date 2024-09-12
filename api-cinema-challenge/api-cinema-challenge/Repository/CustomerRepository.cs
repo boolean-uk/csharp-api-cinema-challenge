5652ef4032170b445d0f709dfe812ca06265bc3b
Malte <malte.stengard@gmail.com>
2024-09-12 11:00:19 +0200

    Customer repo done

--- api-cinema-challenge/api-cinema-challenge/Repository/CustomerRepository.cs
+++ api-cinema-challenge/api-cinema-challenge/Repository/CustomerRepository.cs
@@ -1,4 +1,5 @@
 ﻿using api_cinema_challenge.Models.Customer;
+using Microsoft.EntityFrameworkCore;
 
 namespace api_cinema_challenge.Repository
 {
@@ -6,11 +7,32 @@ namespace api_cinema_challenge.Repository
     {
         public CustomerDTO CreateCustomer(string name, string email, string phone)
         {
-            
+            Customer customer = null;
+            _db.Customers.Add(customer = new Customer() { id = _db.Customers.Max(x => x.id) + 1, name = name, email = email, phone = phone });
+            _db.SaveChanges();
+            return customer.MapToDTO();
         }
 
-        public List<CustomerDTO> GetCustomers() { throw new NotImplementedException(); }
-        public CustomerDTO UpdateCustomer(int id, string name, string email, string phone) { throw new NotImplementedException(); }
-        public CustomerDTO DeleteCustomer(int id) { throw new NotImplementedException(); }
+        public List<CustomerDTO> GetCustomers()
+        {
+            return _db.Customers.ToList().MapListToDTO();
+        }
+        public CustomerDTO UpdateCustomer(int id, string name, string email, string phone)
+        {
+            if (_db.Customers.FirstOrDefault(x => x.id == id) != null)
+            {
+                var customer = new Customer() { id = id, name = name, email = email, phone = phone };
+                _db.Customers.Attach(customer);
+                _db.Customers.Where(c => c.id == id).ExecuteUpdate(x => x.SetProperty(z => z.name, name).SetProperty(x => x.email, email).SetProperty(x => x.phone, phone));
+                return customer.MapToDTO();
+            }
+            return null;
+        }
+        public CustomerDTO DeleteCustomer(int id)
+        {
+            Customer customer = null;
+            _db.Customers.Remove(customer = _db.Customers.FirstOrDefault(x => x.id == id));
+            return customer != null ? customer.MapToDTO() : null;
+        }
     }
 }
