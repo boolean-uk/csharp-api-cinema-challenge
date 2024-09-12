5cceee958c0f56bd840b3a7d7bd45cc520356937
Victor <victor.chicinas@gmail.com>
2024-09-12 11:58:06 +0200

    Error fixing

--- api-cinema-challenge/api-cinema-challenge/Program.cs
+++ api-cinema-challenge/api-cinema-challenge/Program.cs
@@ -22,8 +22,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+//app.UseAuthorization();
+//app.MapControllers();
 app.ConfigureCustomerEndpoint();
 app.ConfigureMovieEndpoint();
 app.ConfigureScreeningEndpoint();
