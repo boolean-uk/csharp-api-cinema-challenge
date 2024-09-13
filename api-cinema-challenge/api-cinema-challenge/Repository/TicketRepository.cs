9f521404e409237bc0f553e104c76c8ee787b9a1
Malte <malte.stengard@gmail.com>
2024-09-13 10:48:49 +0200

    think ticket logic is correct now

--- api-cinema-challenge/api-cinema-challenge/Repository/TicketRepository.cs
+++ api-cinema-challenge/api-cinema-challenge/Repository/TicketRepository.cs
@@ -10,13 +10,13 @@ namespace api_cinema_challenge.Repository
         public TicketDTO BookTicket(int id)
         {
             Ticket ticket = null; // id is wrong, fix
-            if (_db.Tickets.Where(x => x.Id == id).Count() > 0)
+            if (_db.Tickets.Where(x => x.Id == id).Count() > 0 && _db.Tickets.Where(x => x.Id == ticket.Id).Min(x => x.numSeats) - 1 >= 0)
             {
-                _db.Tickets.Add(ticket = new Ticket() { Id = _db.Tickets.Max(x => x.Id) + 1, numSeats = _db.Screenings.FirstOrDefault(x => x.Id == id).Capacity - _db.Tickets.Where(x => x.Id == id).Max(x => x.numSeats) });
+                _db.Tickets.Add(ticket = new Ticket() { Id = id, numSeats = _db.Tickets.Where(x => x.Id == ticket.Id).Min(x => x.numSeats) - 1 });
             }
             else
             {
-                _db.Tickets.Add(ticket = new Ticket() { Id = _db.Tickets.Max(x => x.Id) + 1, numSeats = _db.Screenings.FirstOrDefault(x => x.Id == id).Capacity - 1 });
+                _db.Tickets.Add(ticket = new Ticket() { Id = id, numSeats = _db.Screenings.FirstOrDefault(x => x.Id == ticket.Id).Capacity - 1 });
             }
             return ticket != null ? ticket.MapToDTO() : null;
         }
