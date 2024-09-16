79dcdc7aa8e41dc3139b8541b3eced44fc44e588
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-09-16 13:56:50 +0200

    save

--- api-cinema-challenge/api-cinema-challenge/DTOs/GetCustomerDTO.cs
+++ api-cinema-challenge/api-cinema-challenge/DTOs/GetCustomerDTO.cs
@@ -2,5 +2,12 @@
 {
     public class GetCustomerDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;
+        public List<> Tickets { get; set; }
     }
 }
