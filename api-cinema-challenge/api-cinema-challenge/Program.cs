c49b8f568839797f189ff51ab2cd7148f2e8c077
Muzea001 <muat0072@gmail.com>
2024-10-18 09:12:24 +0200

    Added cors configuration

--- api-cinema-challenge/api-cinema-challenge/Program.cs
+++ api-cinema-challenge/api-cinema-challenge/Program.cs
@@ -13,6 +13,18 @@ builder.Services.AddDbContext<CinemaContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<CinemaContext>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+// 1. Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowS3Bucket", policy =>
+    {
+        policy.WithOrigins("http://aws-muaath-day-1.s3-website.eu-north-1.amazonaws.com") // Replace with your actual S3 bucket URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +34,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// 2. Use CORS middleware
+app.UseCors("AllowS3Bucket");
+
 app.UseHttpsRedirection();
 app.ConfigureCinemApi();
 app.UseHttpsRedirection();
+
 app.Run();
