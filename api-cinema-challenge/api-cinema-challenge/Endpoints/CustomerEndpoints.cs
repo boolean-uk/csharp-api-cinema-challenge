964ca079335be3e6932a41f05b92a180029911a6
magnus195 <22998711+magnus195@users.noreply.github.com>
2025-01-28 12:06:59 +0100

    Cleanup of endpoints

--- api-cinema-challenge/api-cinema-challenge/Endpoints/CustomerEndpoints.cs
+++ api-cinema-challenge/api-cinema-challenge/Endpoints/CustomerEndpoints.cs
@@ -1,4 +1,5 @@
 using api_cinema_challenge.DTO;
+using api_cinema_challenge.Enums;
 using api_cinema_challenge.Models;
 using api_cinema_challenge.Repository;
 using AutoMapper;
@@ -11,19 +12,19 @@ public static class CustomerEndpoints
     public static void ConfigureCustomerEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("customers");
-        
+
         group.MapGet("/", GetCustomers);
     }
-    
+
     [ProducesResponseType(typeof(BaseResponse<IEnumerable<CustomerResponse>>), StatusCodes.Status200OK)]
     private static async Task<IResult> GetCustomers(IRepository<Customer> repository, IMapper mapper)
     {
         var customers = await repository.GetAll();
         var response = new BaseResponse<IEnumerable<CustomerResponse>>(
-            "success",
+            Consts.SuccessStatus,
             mapper.Map<IEnumerable<CustomerResponse>>(customers)
-            );
-        
+        );
+
         return TypedResults.Ok(response);
     }
 }
---
+++ api-cinema-challenge/api-cinema-challenge/Enums/Consts.cs
@@ -0,0 +1,6 @@
+namespace api_cinema_challenge.Enums;
+
+public static class Consts
+{
+    public const string SuccessStatus = "success";
+}
